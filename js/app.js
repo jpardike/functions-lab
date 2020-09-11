99c57fbe4d52993ba7145a67471cea9b8a742cf1
jpardike2 <jpardike@gmail.com>
2020-09-11 15:00:36 -0700

    reverseWordOrder

--- js/app.js
+++ js/app.js
@@ -6,3 +6,12 @@ function printGreeting(name) {
 }
 
 console.log(printGreeting('John'));
+
+// 2. reverseWordOrder
+// Write a function reverseWordOrder that accepts a single argument, a string. The function should return a string with the order of the words reversed. Don't worry about punctuation.
+
+function reverseWordOrder(str) {
+    return str.split(" ").reverse();
+}
+
+console.log(reverseWordOrder('John Hello'));
